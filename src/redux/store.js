52d40110997bd913ceb407546e12e516d0ffa01e
lcsulak <lcsulak@navent.com>
2020-07-05 12:58:15 -0300

    corrijo comentarios

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,14 +6,12 @@ import countryInfoReducer from './countryInfoDucks'
 
 const rootReducer = combineReducers({
 
-    // el pokemones va a ser el array de pokemones. [actualizacion: ahora le agregue en pokeDucks el offset asique ahora va a retorar
-    //                                               el array y el offset (la paginacion)]
-    // el pokeReducer es la funcion que estamos exportando de pokeDucks
-    // y se lo asignamos a pokemones y queda guardada toda la info
+    // el statisticsReducer y countryInfoReducer son las funciones que estamos exportando de countryInfoDucks.js y statisticsDucks.js
+    // y se lo asignamos a statistics y countryInfo y queda guardada toda la info
     
-    // el nombre pokemones es el nombre que va a recibir de la tienda
+    // el nombre statistics y countryInfo es el nombre que va a recibir de la tienda
     // cuando veo mi pagina F12 -> pestaña redux dentro de state -> state
-    // ahi voy a ver que se llama pokemones y dentro todo lo que le asignamos con pokeReducer
+    // ahi voy a ver que se llama pokemones y dentro todo lo que le asignamos con statisticsReducer y countryInfoReducer
 
     // toda esta informacion de statistics y countryInfo la obtengo de todos los ...Ducks.js
     statistics: statisticsReducer,
